9805992b0695a90f8b22c77d80feb886bd7cfb47
David I. Lopez Mejia <inaki.lopez.m@gmail.com>
2015-11-10 17:57:30 +0100

    Added zombies, animations, and pathfinding

---
+++ Assets/Scripts/enemyAI.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class enemyAI : MonoBehaviour
+{
+    public float deathDistance = 0.5f;
+    public float distanceAway;
+    public Transform thisObject;
+    public Transform target;
+    private NavMeshAgent navComponent;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        navComponent = this.gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+
+        float dist = Vector3.Distance(target.position, transform.position);
+
+        if (target)
+        {
+            navComponent.SetDestination(target.position);
+        }
+
+        else
+        {
+            if (target = null)
+            {
+                target = this.gameObject.GetComponent<Transform>();
+            }
+            else
+            {
+                target = GameObject.FindGameObjectWithTag("Player").transform;
+            }
+        }
+        if (dist <= deathDistance)
+        {
+            //KILL PLAYER
+        }
+    }
+}

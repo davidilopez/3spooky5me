5e73544cbd91d8059df03b597bd454342e95b4e5
David I. Lopez Mejia <inaki.lopez.m@gmail.com>
2015-11-17 14:59:16 +0100

    Day and night cycle

---
+++ Assets/enemySight.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemySight : MonoBehaviour {
+
+    public float fieldOfViewAngle = 110f;
+    public bool playerInSight;
+    public Vector3 personalLastSighting;
+
+}
